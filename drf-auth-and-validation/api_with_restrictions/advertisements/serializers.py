9dabca3e1ee5d69271b2905e918b2cec42939351
Denis Kirpichev <kirpichev.1999@mail.ru>
2024-10-27 14:57:55 +0300

    upd fix

--- drf-auth-and-validation/api_with_restrictions/advertisements/serializers.py
+++ drf-auth-and-validation/api_with_restrictions/advertisements/serializers.py
@@ -1,34 +1,25 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-
 from advertisements.models import Advertisement
-
 
 class UserSerializer(serializers.ModelSerializer):
     """Serializer для пользователя."""
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name',
-                  'last_name',)
-
+        fields = ('id', 'username', 'first_name', 'last_name',)
 
 class AdvertisementSerializer(serializers.ModelSerializer):
     """Serializer для объявления."""
 
-    creator = UserSerializer(
-        read_only=True,
-    )
+    creator = UserSerializer(read_only=True)
 
     class Meta:
         model = Advertisement
-        fields = ('id', 'title', 'description', 'creator',
-                  'status', 'created_at',)
+        fields = ('id', 'title', 'description', 'creator', 'status', 'created_at',)
 
     def create(self, validated_data):
         """Метод для создания"""
-
-
         validated_data["creator"] = self.context["request"].user
         return super().create(validated_data)
 
@@ -36,8 +27,17 @@ class AdvertisementSerializer(serializers.ModelSerializer):
         """Метод для валидации. Вызывается при создании и обновлении."""
         user = self.context['request'].user
 
-        if not self.instance:
-            if Advertisement.objects.filter(creator=user).count() > 10:
-                raise serializers.ValidationError(f'User {user} can\'t create more then 10 items!')
+
+        if not self.instance or data.get('status') == 'OPEN':
+
+            open_ads_count = Advertisement.objects.filter(creator=user, status='OPEN').count()
+            if open_ads_count >= 10:
+                raise serializers.ValidationError(f'User  {user} can\'t create more than 10 open items!')
 
         return data
+
+    def update(self, instance, validated_data):
+        """Метод для обновления"""
+
+        self.validate(validated_data)
+        return super().update(instance, validated_data)
